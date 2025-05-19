27232b6d7d07d271cd5f9c2ecb00daa2ac060f0f
BiduBada <artphaune@gmail.com>
2025-05-19 17:05:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -53,10 +53,16 @@ def save_messages(messages):
 #         json.dump(messages, f)
 
 
+@app.route("/new_message", methods=["GET"])
+def new_message():
+    lastLen = int(request.args.get("lastLen"))
+    if len(load_messages()) > lastLen :
+        return str(len(load_messages()))
+    return  str("NO")
 
 @app.route("/messages", methods=["GET"])
 def get_messages():
-    return jsonify(load_messages())
+    return jsonify(load_messages()[-20:])
 
 @app.route("/messages", methods=["POST"])
 def post_message():
